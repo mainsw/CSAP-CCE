ac7c3dc26225f2426bad1a21889a779d3c7c7b85
hjmxodzm <57334764+hjmxodzm@users.noreply.github.com>
2023-05-03 20:44:45 +0900

    Create OT-22.sh

---
+++ openstack/OT-22.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+glance_api_servers=$(grep -Po "(?<=^\[DEFAULT\]\nglance_api_servers = ).*" /etc/nova/nova.conf)
+
+if [[ $glance_api_servers == https://* ]]; then
+    echo "암호화,OT-22,cinder와 glance의 TLS 통신,상,양호" >> openstack_report.csv
+else
+    echo "암호화,OT-22,cinder와 glance의 TLS 통신,상,취약" >> openstack_report.csv
+fi

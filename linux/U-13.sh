61e04d0fd87701ca41730d5633597ba50a6d8f80
bjh2575 <71494499+bjh2575@users.noreply.github.com>
2023-05-03 20:40:18 +0900

    Update U-13.sh

--- linux/U-13.sh
+++ linux/U-13.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 파일 생성 및 초기화
-echo "구분,진단코드,진단항목,취약도,점검결과" > linux_report.csv
-
 # /etc/services 파일 검사
 if [ -O /etc/services ] && [ $(stat -c "%a" /etc/services) -le 644 ]; then
 echo "파일 및 디렉토리 관리,U-13,/etc/services 파일 및 권한 설정,상,양호" >> linux_report.csv

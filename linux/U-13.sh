d2c5c9d6644177760d93244829c44746f483060e
bjh2575 <71494499+bjh2575@users.noreply.github.com>
2023-05-03 21:07:15 +0900

    Update U-13.sh

--- linux/U-13.sh
+++ linux/U-13.sh
@@ -7,7 +7,3 @@ else
 echo "파일 및 디렉토리 관리,U-13,/etc/services 파일 및 권한 설정,상,취약" >> linux_report.csv
 fi
 
-# /etc/srvices 파일 검사
-#if [ ! -O /etc/services ] || [ $(stat -c "%a" /etc/services) -gt 644 #]; then
-#echo "파일 및 디렉토리 관리,U-13,/etc/services 파일 및 권한 설정,상,취약" >> linux_report.csv
-fi

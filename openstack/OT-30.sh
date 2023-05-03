800293c1b790a0d1f1fcf84be7e2218ced30647b
soeyeon <57591478+soeyeon@users.noreply.github.com>
2023-05-03 20:42:15 +0900

    Update OT-30.sh

--- openstack/OT-30.sh
+++ openstack/OT-30.sh
@@ -1,8 +1,6 @@
 export LANG=ko_KR.UTF-8
 #!/bin/bash
 
-echo "구분,진단코드,진단항목,취약도,점검결과"> openstack_report.csv
-
 TMP=$(mktemp)
 cat /etc/keystone/keystone.conf | grep -i "max_request_body_size" > $TMP
 

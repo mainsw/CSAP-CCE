aaa90793695a61e3cd3edda889133f9ee89e632f
hjmxodzm <57334764+hjmxodzm@users.noreply.github.com>
2023-05-06 21:55:41 +0900

    Update OT-19.sh

--- openstack/OT-19.sh
+++ openstack/OT-19.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -f "/etc/nova/nova.conf" ]; then
+  echo "암호화,OT-19,Nova와 Glance의 안전한 통신,상,파일 존재 X" >> openstack_report.csv
+exit
+fi
+
 auth_url=$(grep -Po "(?<=^\[glance\]\nauth_url = ).*" /etc/nova/nova.conf)
 
 if [[ $auth_url == https://* ]]; then

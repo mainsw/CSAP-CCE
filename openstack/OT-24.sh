7b4e3f1aa3aa97e0be5d7147954b93457cafc8e4
hjmxodzm <57334764+hjmxodzm@users.noreply.github.com>
2023-05-07 00:52:21 +0900

    Update OT-24.sh

--- openstack/OT-24.sh
+++ openstack/OT-24.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d "/etc/manila" ]; then
-  echo "암호화,OT-24,공유 파일 시스템 인증을 위한 TLS 활성화,상,파일 존재 X" >> openstack_report.csv
+  echo "암호화,OT-24,공유 파일 시스템 인증을 위한 TLS 활성화,상,N/A" >> openstack_report.csv
   exit
 fi
 

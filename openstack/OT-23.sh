08613bad5ec9efed0e3445fdfdd30f0866952e1f
hjmxodzm <57334764+hjmxodzm@users.noreply.github.com>
2023-05-06 22:01:57 +0900

    Update OT-23.sh

--- openstack/OT-23.sh
+++ openstack/OT-23.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -f "/etc/glance/glance-api.conf" ]; then
+  echo "암호화,OT-23,이미지 스토리지 서비스 인증을 위한 TLS 활성화,상,파일 존재 X" >> openstack_report.csv
+exit
+fi
+
 auth_url=$(grep -Po "(?<=^\[keystone_authtoken\]\nauth_url = ).*" /etc/glance/glance-api.conf)
 
 if [[ $auth_url == https://* ]]; then

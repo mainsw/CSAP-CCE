924dc2164d65659deb13242469d3fbafa0032641
hjmxodzm <57334764+hjmxodzm@users.noreply.github.com>
2023-05-07 00:51:48 +0900

    Update OT-19.sh

--- openstack/OT-19.sh
+++ openstack/OT-19.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f "/etc/nova/nova.conf" ]; then
-  echo "암호화,OT-19,Nova와 Glance의 안전한 통신,상,파일 존재 X" >> openstack_report.csv
+  echo "암호화,OT-19,Nova와 Glance의 안전한 통신,상,N/A" >> openstack_report.csv
 exit
 fi
 

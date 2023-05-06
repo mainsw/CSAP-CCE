984a0d18268608013d05268df9b47053b93649c2
hjmxodzm <57334764+hjmxodzm@users.noreply.github.com>
2023-05-06 21:52:20 +0900

    Update OT-18.sh

--- openstack/OT-18.sh
+++ openstack/OT-18.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -f "/etc/nova/nova.conf" ]; then
+  echo "암호화,OT-18,Compute 인증을 위한 보안프로토콜 사용,상,파일 존재 X" >> openstack_report.csv
+exit
+fi
+
 auth_url=$(grep -Po "(?<=^\[keystone_authtoken\]\nauth_url = ).*" /etc/nova/nova.conf)
 
 if [[ $auth_url == https://* ]]; then

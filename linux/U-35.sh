11d7a0cb240fe57861edca96ad3aaffac2036cc2
Simjeongbo <81142843+Simjeongbo@users.noreply.github.com>
2023-05-07 20:15:34 +0900

    Update U-35.sh

--- linux/U-35.sh
+++ linux/U-35.sh
@@ -6,7 +6,7 @@
 
 #진단방법:
 #패치 적용 정책 수립 여부 및 정책에 따른 패치 적용 여부 확인 
-https://github.com/mainsw/CSAP-CCE/blob/main/linux/U-35.sh
+#https://github.com/mainsw/CSAP-CCE/blob/main/linux/U-35.sh
 #조치방법:
 #LINUX는 서버에 설치된 패치 리스트의 관리가 불가능하므로 rpm 패키지별 버그가 Fix된 최신 버전 설치가 필요함
 #LINUX는 오픈되고, 커스터마이징 된 OS이므로 LINUX를 구입한 벤더에 따라 rpm 패키지가 다를 수 있으며, 아래의 사이트는 RedHat LINUX에 대한 버그 Fix 관련 사이트임

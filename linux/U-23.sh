eac349a947cfba71fb0abe29f65552cf11c6bc15
hjmxodzm <57334764+hjmxodzm@users.noreply.github.com>
2023-05-09 14:45:43 +0900

    22번23번수정
    22번23번수정

--- linux/U-23.sh
+++ linux/U-23.sh
@@ -66,23 +66,22 @@ if [  -d "/etc/xinetd.d" ]; then
 fi
 
 
-if [ ! -d "/etc/xinetd.d" ]; then
-	ls /etc/inetd.conf* >/dev/null 2>&1
-	if  [ $? != 0 ] ; then 
-		echo ''
-	else
-		for i in 'ls /etc/inetd.conf*'
-		do
-			if grep -qE "echo|discard|daytime|chargen" /etc/inetd.conf; then
-				if ! grep -qE '^#.*rsh|^#.*rlogin|^#.*rexec|^rsh|^rlogin|^rexec' /etc/inetd.conf; then
-					echo -e "서비스 관리,U-23,DOS 공격에 취약한 서비스 비활성화,상,양호" >> linux_report.csv
-				else
-					echo -e "서비스 관리,U-23,DOS 공격에 취약한 서비스 비활성화,상,취약" >> linux_report.csv
-				fi
-			else
+ls /etc/inetd.conf* >/dev/null 2>&1
+if  [ $? != 0 ] ; then 
+	echo ''
+else
+	for i in 'ls /etc/inetd.conf*'
+	do
+		if grep -qE "echo|discard|daytime|chargen" /etc/inetd.conf; then
+			if ! grep -qE '^#.*rsh|^#.*rlogin|^#.*rexec|^rsh|^rlogin|^rexec' /etc/inetd.conf; then
 				echo -e "서비스 관리,U-23,DOS 공격에 취약한 서비스 비활성화,상,양호" >> linux_report.csv
+			else
+				echo -e "서비스 관리,U-23,DOS 공격에 취약한 서비스 비활성화,상,취약" >> linux_report.csv
 			fi
-		done
-	fi
+		else
+			echo -e "서비스 관리,U-23,DOS 공격에 취약한 서비스 비활성화,상,양호" >> linux_report.csv
+		fi
+	done
+fi
 exit
 fi

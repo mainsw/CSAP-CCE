df7593ce2eb0a49ab25dca9bdaa8ce84a2dbf778
mainsw <82371999+mainsw@users.noreply.github.com>
2023-05-11 17:34:53 +0900

    OpenStack script 1~14

--- openstack/OT-02.sh
+++ openstack/OT-02.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# 점검 파일 경로
 FILES=( "/etc/keystone/keystone.conf" \
         "/etc/keystone/keystone-paste.ini" \
         "/etc/keystone/policy.json" \
@@ -9,8 +8,6 @@ FILES=( "/etc/keystone/keystone.conf" \
         "/etc/keystone/ssl/private/signing_key.pem" \
         "/etc/keystone/ssl/certs/ca.pem" )
 
-OWNER="keystone"
-GROUP="keystone"
 VULN=false
 found_file=false
 
@@ -23,27 +20,26 @@ for file in "${FILES[@]}"; do
 done
 
 if [[ "$found_file" == false ]]; then
-    echo "[OT-01] N/A: No files to check."
+    echo "[OT-02] N/A: No files to check."
 		if [ -e "openstack_report.csv" ]; then
-			echo "파일 권한 관리,OT-01,Identity 설정파일 소유권 설정,상,N/A" >> openstack_report.csv
-			echo "[OT-01] Report generated."
+			echo "파일 권한 관리,OT-02,Identity 설정파일 접근권한 설정,상,N/A" >> openstack_report.csv
+			echo "[OT-02] Report generated."
 		else
 			echo "구분,진단 코드,진단 항목,취약도,점검 결과" > openstack_report.csv
-			echo "파일 권한 관리,OT-01,Identity 설정파일 소유권 설정,상,N/A" >> openstack_report.csv
-			echo "[OT-01] Report generated."
+			echo "파일 권한 관리,OT-02,Identity 설정파일 접근권한 설정,상,N/A" >> openstack_report.csv
+			echo "[OT-02] Report generated."
 		fi
     exit 1
 fi
 
-
 for FILEPATH in "${FILES[@]}"
 do
-    if [ -e "$FILEPATH" ]; then # 파일이 존재하면 작동함.
-        # 오너와 그룹이 모두 keystone이면 safe, 아니면 vuln.
-        if [[ "$(stat -c '%U' "$FILEPATH")" == "$OWNER" && "$(stat -c '%G' "$FILEPATH")" == "$GROUP" ]]; then
-            echo "[OT-01] $FILEPATH is safe"
+    if [ -e "$FILEPATH" ]; then
+        PERMISSIONS=$(stat -c '%a' "$FILEPATH")
+        if [ "$PERMISSIONS" -ge 640 ]; then
+            echo "[OT-02] $FILEPATH is safe"
         else
-            echo "[OT-01] $FILEPATH is vulnerable"
+            echo "[OT-02] $FILEPATH is vulnerable"
             VULN=true
         fi
     fi
@@ -51,19 +47,19 @@ done
 
 # VULN 값에 따라 취약 / 양호 레포트 작성.
 if [ "$VULN" = true ]; then
-    REPORT="파일 권한 관리,OT-01,Identity 설정파일 소유권 설정,상,취약"
+    REPORT="파일 권한 관리,OT-02,Identity 설정파일 접근권한 설정,상,취약"
 else
-    REPORT="파일 권한 관리,OT-01,Identity 설정파일 소유권 설정,상,양호"
+    REPORT="파일 권한 관리,OT-02,Identity 설정파일 접근권한 설정,상,양호"
 fi
 
 # 파일이 존재하면 이어서 작성, 존재하지 않으면 보고서 헤더 작성 후 레포트 작성.
 if [ -e "openstack_report.csv" ]; then
     echo "$REPORT" >> openstack_report.csv
-    echo "[OT-01] Report generated."
+    echo "[OT-02] Report generated."
 else
     echo "구분,진단 코드,진단 항목,취약도,점검 결과" > openstack_report.csv
     echo "$REPORT" >> openstack_report.csv
-    echo "[OT-01] Report generated."
+    echo "[OT-02] Report generated."
 fi
 
 exit 0

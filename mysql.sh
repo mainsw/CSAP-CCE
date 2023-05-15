3db8146d88b1cd79b1fce75acbd64c218772e8d2
mainsw <82371999+mainsw@users.noreply.github.com>
2023-05-16 04:03:01 +0900

    실행 스크립트 항목 추가

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 실행할 스크립트 파일들을 배열에 추가
-scripts=(DY-01.sh DY-02.sh DY-03.sh DY-04.sh DY-05.sh DY-06.sh)
+scripts=(DY-01.sh DY-02.sh DY-03.sh DY-04.sh DY-05.sh DY-06.sh DY-07.sh DY-08.sh DY-09.sh)
 
 # 배열에 추가한 스크립트 파일들을 차례대로 실행
 for script in "${scripts[@]}"

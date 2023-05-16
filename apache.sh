7d047b2594e3d1b397aa11259e0de0aa3418fedd
soeyeon <57591478+soeyeon@users.noreply.github.com>
2023-05-16 12:19:01 +0900

    apache

---
+++ apache.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 실행할 스크립트 파일들을 배열에 추가
+scripts=(AP-01.sh AP-02.sh AP-03.sh AP-04.sh AP-05.sh AP-06.sh)
+
+# 배열에 추가한 스크립트 파일들을 차례대로 실행
+for script in "${scripts[@]}"
+do
+    bash "./apache/$script"
+done

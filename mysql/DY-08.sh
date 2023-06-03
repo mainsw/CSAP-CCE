81e54191c98c50a28ba41cd49e6921937429d606
Simjeongbo <jjungbo38@gmail.com>
2023-06-03 22:47:27 +0900

    mysql 인자값 설정

--- mysql/DY-08.sh
+++ mysql/DY-08.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # General log 설정 확인
-general_log=$(mysql -u root -p -e "show variables like 'general_log%';" | grep -c "general_log.*ON\|general_log.*1")
+general_log=$(mysql -u root -p$pass -e "show variables like 'general_log%';" | grep -c "general_log.*ON\|general_log.*1")
 # Slow Query 설정 확인
-slow_query_log=$(mysql -u root -p -e "show variables like 'slow_query_log%';" | grep -c "slow_query_log.*ON")
+slow_query_log=$(mysql -u root -p$pass -e "show variables like 'slow_query_log%';" | grep -c "slow_query_log.*ON")
 
 if [ $general_log -eq 1 ]; then
   if [ $slow_query_log -eq 1 ]; then

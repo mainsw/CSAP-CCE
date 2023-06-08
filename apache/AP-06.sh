69e79fef1b588d6c8f820bd83f46d67aebb651c2
hjmxodzm <57334764+hjmxodzm@users.noreply.github.com>
2023-06-09 02:14:52 +0900

    아파치 수정했습니다

--- apache/AP-06.sh
+++ apache/AP-06.sh
@@ -4,7 +4,7 @@ export LANG=ko_KR.UTF-8
 TMP=$(ps -ef | grep httpd | awk '{print $1}' )
 
 if echo "$TMP" | grep -q 'root'  ; then
-	echo "보안설정,AP-06,웹 프로세스 권한 제한,상,취약">> apache_report.csv
+	echo "접근관리,AP-06,웹 프로세스 권한 제한,상,취약">> apache_report.csv
 else
-	echo "보안설정,AP-06,웹 프로세스 권한 제한,상,양호">> apache_report.csv
+	echo "접근관리,AP-06,웹 프로세스 권한 제한,상,양호">> apache_report.csv
 fi

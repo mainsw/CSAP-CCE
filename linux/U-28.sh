2494c938a532730cd01435b2e57eee979c42dacc
soeyeon <57591478+soeyeon@users.noreply.github.com>
2023-06-11 20:32:50 +0900

    , 제거

--- linux/U-28.sh
+++ linux/U-28.sh
@@ -8,8 +8,8 @@ ps -ef | egrep "ypserv|ypbind|ypxfrd|rpc.yppasswdd|rpc.ypupdated" | grep -v "gre
 #문자열 길이가 0이 아니면 참
 if [ -n $TMP ] ; then
 	# "NIS, NIS+ 서비스가 구동 중이지 않을 경우
-	echo "서비스 관리,U-28,\"NIS, NIS+ 점검\",상,양호">> linux_report.csv
+	echo "서비스 관리,U-28,NIS NIS+ 점검,상,양호">> linux_report.csv
 else
 	# "NIS, NIS+ 서비스가 구동 중일 경우
-	echo "서비스 관리,U-28,\"NIS, NIS+ 점검\",상,취약">> linux_report.csv
+	echo "서비스 관리,U-28,NIS NIS+ 점검,상,취약">> linux_report.csv
 fi
